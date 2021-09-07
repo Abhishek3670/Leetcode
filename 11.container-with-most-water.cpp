d192b363e0f511fdef8f12a156a141c3b24952b7
Abhishek Sharma <aatish3670@gmail.com>
2021-09-08 00:36:38 +0530

    first commit

---
+++ 11.container-with-most-water.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=11 lang=cpp
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0 ;
+        int l = 0 , r = height.size() - 1;
+        while (l < r)
+        {
+            int h = min(height[l] , height[r]);
+            ans = max(ans , (r-l)*h);
+            while(height[l] <= h && l < r)  l++;
+            while(height[r] <= h && l < r)  r--;
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
